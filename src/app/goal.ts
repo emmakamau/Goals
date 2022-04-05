eab802903a7ebb3249bde39b041b19c6b574960d
EmmaK <emmaculatewkamau@gmail.com>
2022-04-05 10:59:12 +0300

    Add toggle effect to goals

--- src/app/goal.ts
+++ src/app/goal.ts
@@ -1,5 +1,7 @@
 export class Goal {
-    id: number;
-    name: string;
-    description: string;
+    showDescription: boolean;
+  constructor(public id: number,public name: string,public description: string){
+    this.showDescription=false;
+  }
 }
+
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,11 +8,17 @@ import { Goal } from '../goal'
 })
 export class GoalComponent implements OnInit {
   goals:Goal[] = [
-    {id:1, name:'Watch finding Nemo',description:'We can now add descriptions to our goals in the array'},
-    {id:2, name:'Teach a class',description:'We can now add descriptions to our goals in the array'},
-    {id:3, name:'Make some food',description:'We can now add descriptions to our goals in the array'},
-    {id:4, name:'Study Angular',description:'We can now add descriptions to our goals in the array'}
+    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
+    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
+    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon'),
+    new Goal(4,'Get Dog Food','Pupper likes expensive snacks'),
+    new Goal(5,'Solve math homework','Damn Math'),
+    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord'),
   ];
+
+  toggleDetails(index){
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
 
   constructor() { }
 
